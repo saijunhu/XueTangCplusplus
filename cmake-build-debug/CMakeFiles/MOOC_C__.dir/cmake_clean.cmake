e17a354ce5b0a34545941f4df5e5bd1c867bfee5
stevehu <1547963731@qq.com>
2018-12-27 09:52:40 +0800

    INITIALIZE

---
+++ cmake-build-debug/CMakeFiles/MOOC_C__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MOOC_C__.dir/main.cpp.o"
+  "MOOC_C__.pdb"
+  "MOOC_C__"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MOOC_C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
